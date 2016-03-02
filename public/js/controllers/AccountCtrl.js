67031644c96b7d2ea027b32d760c971a070cd248
Michael Staehling <Michael.staehling@gmail.com>
2016-03-01 21:53:32 -0500

    added account page and controller

---
+++ public/js/controllers/AccountCtrl.js
@@ -0,0 +1,13 @@
+app.controller('FeedCtrl', ["$routeParams",
+function ($routeParams, $http) {
+    var vm = this;
+
+    vm.login = function(){
+
+    }
+
+    vm.signUp = function (){
+      
+    }
+
+}]);
