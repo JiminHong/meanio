ea4ff03a4744193cea0b596fb15d711e6ffd73a8
Paul Diederichs <pauldiederichs95@gmail.com>
2016-03-01 20:16:41 -0500

    server works

--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,12 @@
-// module.exports = function(app) {
-//
-// var Course = require('../models/courses.js');
-//
-// // Inserts a new course
-// app.post('/api/newCourse', function(req, res){
-//     Course.create(req.body, function(results){
-// 	    res.status(201).send(results);
-//     });
-// });
-// // Finds all of the courses by the degree abbreviation
-// app.get('/api/fetchCourses/:degreeAbbr', function(req, res){
-//     Course.fetchAll(req.params, function(doc){
-// 	    res.send(doc);
-//     });
-// });
-//
-// // Finds one course by degree abbreviation and course abbreviation
-// app.get('/api/fetchCourse/:degreeAbbr/:abbr', function(req, res){
-//     Course.fetch(req.params, function(doc){
-//     	res.send(doc);
-//     });
-// });
-//
-// } //end export
+module.exports = function(app) {
+
+var bodyParser = require('body-parser');
+
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',function(req,res){
+    res.render('home');
+});
+};
