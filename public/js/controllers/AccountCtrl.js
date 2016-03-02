0cd6c2faea85a7e56d419a3d8d43b7ceb4442265
Michael Staehling <Michael.staehling@gmail.com>
2016-03-01 22:17:40 -0500

    bases for sign up

--- public/js/controllers/AccountCtrl.js
+++ public/js/controllers/AccountCtrl.js
@@ -1,13 +1,13 @@
-app.controller('FeedCtrl', ["$routeParams",
+app.controller('AccountCtrl',
 function ($routeParams, $http) {
     var vm = this;
 
     vm.login = function(){
-
+console.log('hi');
     }
 
     vm.signUp = function (){
-      
+console.log('hi');
     }
 
-}]);
+});
